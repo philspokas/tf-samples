8d4450122b482d2eb7ee4e4cd31dc03acb1a73d6
Phil Spokas <pspokas@micron.com>
2020-10-14 06:44:11 -0700

    initial tests

--- logicapps/main.tf
+++ logicapps/main.tf
@@ -4,8 +4,9 @@ variable subscription_id {}
 variable client_secret {}
 variable input_resource_group_name {}
 
+
 provider "azurerm" {
-    version = "2.29"
+    version = "2.30"
     features {}   
 }
 
@@ -20,25 +21,31 @@ provider "azurerm" {
 #     client_secret            = var.client_secret
 # }
 
-module "logicappworkflow" {
-    source                   = "git::https://bitbucket.micron.com/bbdc/scm/cloudstds/azure.git//terraform/logicappworkflow?ref=features/logic-app-workflow"
-    workflow_name            = "la-pcstest-01"
-    resource_group_name      = var.input_resource_group_name
-    subscription_id          = var.subscription_id
-    client_id                = var.client_id
-    tenant_id                = var.tenant_id
-    client_secret            = var.client_secret
-}
+# module "logicappworkflow" {
+#     source                   = "git::https://bitbucket.micron.com/bbdc/scm/cloudstds/azure.git//terraform/logicappworkflow?ref=features/logic-app-workflow"
+#     workflow_name            = "la-pcstest-01"
+#     resource_group_name      = var.input_resource_group_name
+#     subscription_id          = var.subscription_id
+#     client_id                = var.client_id
+#     tenant_id                = var.tenant_id
+#     client_secret            = var.client_secret
+# }
+
+#    source                      = "git::https://bitbucket.micron.com/bbdc/scm/cloudstds/azure.git//terraform/servicebus?ref=features/logic-app-workflow"
+
 
 module "servicebus" {
-    source              = "git::https://bitbucket.micron.com/bbdc/scm/cloudstds/azure.git//terraform/servicebus?ref=features/logic-app-workflow"
-    namespace_name      = "sb-pcstest-01"
-    resource_group_name = var.input_resource_group_name
-    subnet_id           = ""
-    subscription_id     = var.subscription_id
-    client_id           = var.client_id
-    tenant_id           = var.tenant_id
-    client_secret       = var.client_secret 
+    source                      = "git::https://bitbucket.micron.com/bbdc/scm/cloudstds/azure.git//terraform/servicebus?ref=updates/service-bus-private-link"
+    namespace_name              = "sb-pcstest-02"
+    resource_group_name         = var.input_resource_group_name
+    subscription_id             = var.subscription_id
+    client_id                   = var.client_id
+    tenant_id                   = var.tenant_id
+    client_secret               = var.client_secret 
+    vnet_resource_group_name    = var.input_resource_group_name
+    vnet_name                   = "vnet-dev-vmtest"
+    subnet_name                 = "subnet-jmtest"
+    private_endpoint_enabled    = "true"
 }
 
 
---
+++ logicapps/readme.md
@@ -0,0 +1,15 @@
+
+# Logic App Terraform Modules Samples
+
+Preconditions:
+
+VNet and subnet must exist
+
+Microsoft.ServiceBus to subnet's ServiceEndpoints
+
+
+Service bus inputqueue and outputqueue
+Logic app 
+
+Set access key on logic app
+Endpoint=sb://sbs-sb-pcstest-02-wu2-7804xlxj-001.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=3XmKRrm/PnlPfl/SPaRzEO5rZINbxCRxjZ3EOIBVun0=
