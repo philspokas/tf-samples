74f43815aae14c514d2272ec2c3749256a16b32c
Phil Spokas <pspokas@micron.com>
2020-10-27 16:15:29 -0700

    get modules from main

--- logicapps/main.tf
+++ logicapps/main.tf
@@ -4,29 +4,25 @@ variable subscription_id {}
 variable client_secret {}
 variable input_resource_group_name {}
 
-
 provider "azurerm" {
     version = "2.30"
     features {}   
 }
 
-
-# module "logicappworkflow" {
-#     source                   = "git::https://bitbucket.micron.com/bbdc/scm/cloudstds/azure.git//terraform/logicappworkflow?ref=features/logic-app-workflow"
-#     workflow_name            = "la-pcstest-01"
-#     resource_group_name      = var.input_resource_group_name
-#     subscription_id          = var.subscription_id
-#     client_id                = var.client_id
-#     tenant_id                = var.tenant_id
-#     client_secret            = var.client_secret
-# }
-
-#    source                      = "git::https://bitbucket.micron.com/bbdc/scm/cloudstds/azure.git//terraform/servicebus?ref=features/logic-app-workflow"
-
+module "logicappworkflow" {
+    source                   = "git::https://bitbucket.micron.com/bbdc/scm/cloudstds/azure.git//terraform/logicappworkflow"
+    workflow_name            = "la-pcstest-03"
+    resource_group_name      = var.input_resource_group_name
+    subscription_id          = var.subscription_id
+    client_id                = var.client_id
+    tenant_id                = var.tenant_id
+    client_secret            = var.client_secret
+    use_integration_service  = false
+}
 
 module "servicebus" {
-    source                      = "git::https://bitbucket.micron.com/bbdc/scm/cloudstds/azure.git//terraform/servicebus?ref=updates/service-bus-private-link"
-    namespace_name              = "sb-pcstest-02"
+    source                      = "git::https://bitbucket.micron.com/bbdc/scm/cloudstds/azure.git//terraform/servicebus"
+    namespace_name              = "sb-pcstest-03"
     resource_group_name         = var.input_resource_group_name
     subscription_id             = var.subscription_id
     client_id                   = var.client_id
@@ -37,6 +33,3 @@ module "servicebus" {
     subnet_name                 = "subnet-jmtest"
     private_endpoint_enabled    = "true"
 }
-
-
-
