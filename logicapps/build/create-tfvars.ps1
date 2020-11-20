1dd5089925b6d71805c99dee85c06ed657ef2950
Phil Spokas <pspokas@micron.com>
2020-11-20 10:44:19 -0800

    create and use tfvars

---
+++ logicapps/build/create-tfvars.ps1
@@ -0,0 +1,8 @@
+$vars = @(
+    "workflow_name=`"$($env:workflow_name)`"",
+    "resource_group_name=`"$($env:resoruce_group_name)`"",
+    "vnet_resource_group_name=`"$($env:vnet_resource_group_name)`"",
+    "vnet_name=`"$($env:vnet_name))`"",
+    "use_integration_service=`"$($env:use_integration_service))`""
+)
+Set-Content -Path ".\main.tfvars" -Value $vars
