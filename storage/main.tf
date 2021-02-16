a78277c9dce51269dc442c1bc2a444c39033703e
Phil Spokas <pspokas@micron.com>
2021-02-16 06:58:14 -0800

    no private endpoint

--- storage/main.tf
+++ storage/main.tf
@@ -22,6 +22,7 @@ module "storageaccount" {
     source                   = "git::https://bitbucket.micron.com/bbdc/scm/cloudstds/azure.git//terraform/storageaccount?ref=storageaccount/1.0.1"
     storage_name             = var.storage_name
     storage_type             = ["blob"]
+    private_endpoint_enabled  = false
     resource_group_name      = var.resource_group_name
     subscription_id          = var.subscription_id
     client_id                = var.client_id
