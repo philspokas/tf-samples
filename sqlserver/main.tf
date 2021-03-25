1ceefe9bd287a8cd3710cf53e34b0ec6ab2d454f
Phil Spokas <pspokas@micron.com>
2021-03-25 15:24:38 -0700

    set default provider cres

--- sqlserver/main.tf
+++ sqlserver/main.tf
@@ -23,6 +23,10 @@ terraform {
 }
 
 provider "azurerm" {
+    subscription_id            = var.subscription_id
+    client_secret              = var.client_secret
+    client_id                  = var.client_id
+    tenant_id                  = var.tenant_id
     features {}
 }
 
