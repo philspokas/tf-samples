1f805942e3d9e373dde51a9409c5f97d1847c24e
Phil Spokas <pspokas@micron.com>
2021-03-30 13:29:28 -0700

    fix db name var

--- sqlserver/build/create-tfvars.ps1
+++ sqlserver/build/create-tfvars.ps1
@@ -1,7 +1,7 @@
 $vars = @(
     "input_sql_server_name=`"$($env:sql_server_name)`"",
     "input_server_admin_login=`"$($env:server_admin_login)`"",
-    "input_database_name=`"$($env:input_database_name)`"",
+    "input_database_name=`"$($env:database_name)`"",
     "input_resource_group_name=`"$($env:resource_group_name)`"",
     "input_vnet_resource_group_name=`"$($env:vnet_resource_group_name)`"",
     "input_vnet_name=`"$($env:vnet_name)`"",
